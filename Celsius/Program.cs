9036b9477130e1b0089d2175d5b36b530b5996e3
David Mesa H <105566843+davidmesah@users.noreply.github.com>
2023-05-15 16:08:53 -0500

    Calculadora Celsius

---
+++ Celsius/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Celsius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c=0,f=0,cons=9/5;
+            Console.WriteLine("Ingrese los grados centigrados");
+            c= double.Parse(Console.ReadLine());
+            //f=((double)9/5)*c+32;
+            f=1.8*cons*c+32;
+            Console.WriteLine("{0} grados centigrados, equivalen a {1} grados farenheit",f);
+        }
+    }
+}
